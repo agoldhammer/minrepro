4a4323d14d1cad64dbff1e6cc2b756c518362efd
Art Goldhammer <art.goldhammer@gmail.com>
2022-01-13 14:05:02 -0500

    demo done

--- src/main/demo.cljs
+++ src/main/demo.cljs
@@ -1,3 +1,19 @@
 (ns demo)
 
 (println "hello")
+
+(defn print-with-delay
+  [delay]
+  (let [timer-id (js/setTimeout
+                  #(println "delayed" delay "ms") delay)]
+    timer-id))
+
+(defn print-to-maxdelay
+  [maxdelay]
+  (doseq [delay (range 1 maxdelay)]
+    (print-with-delay delay)))
+
+(defn -main
+  [& args]
+  (print-to-maxdelay 50)
+  (println "hello from main" args))
